c35192166d152e7dad8b11a4c75ee9a2e8a078e8
Anton Farrugia <anton@Antons-MacBook-Pro.local>
2021-02-24 19:01:38 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Anton's World!'));
+app.get('/', (req, res) => res.send('Testing the pipeline approval process));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
